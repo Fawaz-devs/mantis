b269c964cdeadeb9209a54843bdf6ebf68504d73
ash-hashtag <hashtag438@gmail.com>
2024-03-08 20:49:22 +0530

    can call libc functions now

--- src/libc/libc.rs
+++ src/libc/libc.rs
@@ -27,16 +27,10 @@ pub fn malloc_test() -> anyhow::Result<Vec<u8>> {
 
     // Defining Malloc Signature
     let libc_malloc_fn_id = {
-        // let mut signature = module.make_signature();
         ctx.func.signature.params.push(AbiParam::new(types::I64));
         ctx.func.signature.returns.push(AbiParam::new(types::I64));
-        // ctx.func.signature = signature.clone();
         let func_id = module.declare_function("malloc", Linkage::Import, &ctx.func.signature)?;
-        // let sig = ctx.func.signature.clone();
         let mut builder = FunctionBuilder::new(&mut ctx.func, &mut fn_builder_ctx);
-        // builder
-        //     .func
-        //     .declare_imported_user_function(UserExternalName::new(0, func_id.as_u32()));
         let entry_block = builder.create_block();
         builder.append_block_params_for_function_params(entry_block);
         builder.switch_to_block(entry_block);
@@ -46,35 +40,36 @@ pub fn malloc_test() -> anyhow::Result<Vec<u8>> {
         let result = builder.inst_results(inst)[0];
         builder.ins().return_(&[result]);
 
-        // for (idx, value) in builder
-        //     .block_params(entry_block)
-        //     .to_vec()
-        //     .iter()
-        //     .enumerate()
-        // {
-        //     let var = Variable::new(idx);
-        //     builder.declare_var(var, sig.params[idx].value_type);
-
-        //     builder.def_var(var, *value);
-        // }
-
-        // let malloc_func_id = func_id;
-        // print!(
-        //     "{:?}\n",
-        //     module.declarations().get_function_decl(malloc_func_id)
-        // );
-        // let malloc_func_ref = FuncRef::from_u32(malloc_func_id.as_u32());
-        // let malloc_size = builder.block_params(entry_block)[0];
-        // let call_inst = builder.ins().call(malloc_func_ref, &[malloc_size]);
-        // let returned_value = builder.inst_results(call_inst)[0];
-        // builder.ins().return_(&[returned_value]);
-        // let value = builder.ins().iconst(types::I64, 1025);
-        // builder.ins().return_(&[value]);
-
         builder.seal_all_blocks();
         builder.finalize();
 
         let id = module.declare_function("libc_malloc", Linkage::Local, &ctx.func.signature)?;
+
+        module.define_function(id, &mut ctx)?;
+
+        module.clear_context(&mut ctx);
+
+        id
+    };
+
+    let libc_puts_fn_id = {
+        ctx.func.signature.params.push(AbiParam::new(types::I64));
+        ctx.func.signature.returns.push(AbiParam::new(types::I32));
+        let func_id = module.declare_function("puts", Linkage::Import, &ctx.func.signature)?;
+        let mut builder = FunctionBuilder::new(&mut ctx.func, &mut fn_builder_ctx);
+        let entry_block = builder.create_block();
+        builder.append_block_params_for_function_params(entry_block);
+        builder.switch_to_block(entry_block);
+        let func_ref = module.declare_func_in_func(func_id, &mut builder.func);
+        let char_ptr = builder.block_params(entry_block)[0];
+        let inst = builder.ins().call(func_ref, &[char_ptr]);
+        let result = builder.inst_results(inst)[0];
+        builder.ins().return_(&[result]);
+
+        builder.seal_all_blocks();
+        builder.finalize();
+
+        let id = module.declare_function("libc_puts", Linkage::Local, &ctx.func.signature)?;
 
         module.define_function(id, &mut ctx)?;
 
@@ -93,25 +88,23 @@ pub fn malloc_test() -> anyhow::Result<Vec<u8>> {
         let entry_block = builder.create_block();
         builder.append_block_params_for_function_params(entry_block);
         builder.switch_to_block(entry_block);
-        // for (idx, value) in builder
-        //     .block_params(entry_block)
-        //     .to_vec()
-        //     .iter()
-        //     .enumerate()
-        // {
-        //     let var = Variable::new(idx);
-        //     builder.declare_var(var, signature.params[idx].value_type);
-
-        //     builder.def_var(var, *value);
-        // }
 
         let argc = builder.block_params(entry_block)[0];
         let size_val = builder.ins().iconst(types::I64, 1024);
         let func_ref = module.declare_func_in_func(libc_malloc_fn_id, &mut builder.func);
         let call_inst = builder.ins().call(func_ref, &[size_val]);
         let malloc_ptr = builder.inst_results(call_inst)[0];
-        let malloc_ptr = builder.ins().ireduce(types::I32, malloc_ptr);
-        builder.ins().return_(&[malloc_ptr]);
+        let const_str = "Hello World\n\0";
+        for (i, c) in const_str.bytes().enumerate() {
+            let val = builder.ins().iconst(types::I8, c as i64);
+            builder
+                .ins()
+                .store(MemFlags::trusted(), val, malloc_ptr, i as i32);
+        }
+        let func_ref = module.declare_func_in_func(libc_puts_fn_id, &mut builder.func);
+        let call_inst = builder.ins().call(func_ref, &[malloc_ptr]);
+        let puts_result = builder.inst_results(call_inst)[0];
+        builder.ins().return_(&[puts_result]);
         builder.seal_all_blocks();
         builder.finalize();
         let func_id = module.declare_function("main", Linkage::Export, &signature)?;
